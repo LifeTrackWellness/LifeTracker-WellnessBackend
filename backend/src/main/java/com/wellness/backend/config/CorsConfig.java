81aaa11ca9f83020d35cda288c143f271296646d
andre <andrea.marind.1@gmail.com>
2026-04-20 20:05:20 -0500

    Cambio en el CorsConfig

--- backend/src/main/java/com/wellness/backend/config/CorsConfig.java
+++ backend/src/main/java/com/wellness/backend/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.wellness.config;
+package com.wellness.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
